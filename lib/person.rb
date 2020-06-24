5f9aa1889b3ab771c81dc8aa9697985e86843063
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-06-24 14:48:18 -0400

    THE FINAL PRODUCT

--- lib/person.rb
+++ lib/person.rb
@@ -12,6 +12,18 @@ class Person
         @hygiene=8
     end
 
+    def happiness
+        @happiness = 10 if @happiness > 10 
+        @happiness = 0 if @happiness < 0
+        @happiness
+    end
+
+    def hygiene
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0
+        @hygiene
+    end
+
     def clean?
         self.hygiene>7
     end
@@ -21,41 +33,42 @@ class Person
     end
 
     def get_paid(salary)
-        self.balance+=salary
+        self.bank_account+=salary
         "all about the benjamins"
     end
 
     def take_bath
-        if hygiene < 7
-            self.hygiene += 4
-            "♪ Rub-a-dub just relaxing in the tub ♫"
-        end    
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
     end
     
     def work_out
-        if hygiene >= 3
-            self.hygiene +- 3
-        end
-        
-        if happiness <= 8
-            self.happiness += 2
-        end
-
+        self.hygiene -= 3
+        self.happiness += 2
         "♪ another one bites the dust ♫"
     end
 
     def call_friend(friend)
-        
-        if self.happiness <= 7
-            self.happiness += 3
-        end
-        
-        if friend.happiness <= 7
-            friend.happiness += 3
-        end
-        #binding.pry
-
+        self.happiness += 3
+        friend.happiness <= 7
+        friend.happiness += 3
         "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        
+        case topic 
+            when "politics"
+                self.happiness -= 2
+                friend.happiness -= 2
+                "blah blah partisan blah lobbyist"
+            when "weather"
+                self.happiness += 1
+                friend.happiness += 1
+                "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
 
     end
 
