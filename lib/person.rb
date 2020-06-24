a7171e997b7721c32d4066db5e29cddd74ff56bf
Fraciscv <Francisco.Andaur@icloud.com>
2020-06-24 15:43:15 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -34,24 +34,24 @@ class Person
 
   def get_paid(salary)
     self.bank_account += salary
-    p "all about the benjamins"
+     "all about the benjamins"
   end
 
   def take_bath
     self.hygiene += 4
-    p "♪ Rub-a-dub just relaxing in the tub ♫"
+     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out 
     self.happiness += 2
     self.hygiene -= 3
-    p "♪ another one bites the dust ♫"
+     "♪ another one bites the dust ♫"
   end
 
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
-    p "Hi #{friend.name}! It's #{self.name}. How are you?"
+     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
   def start_conversation(person, topic)
@@ -59,13 +59,13 @@ class Person
     when "politics"
          self.happiness -= 2
          person.happiness -= 2
-         p "blah blah partisan blah lobbyist"
+          "blah blah partisan blah lobbyist"
     when "weather"
         self.happiness += 1
         person.happiness += 1
-        p "blah blah sun blah rain"
+         "blah blah sun blah rain"
     else 
-        p "blah blah blah blah blah"
+         "blah blah blah blah blah"
         end
     end
 end
