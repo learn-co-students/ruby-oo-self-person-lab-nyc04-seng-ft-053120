e154dd1830a7943d11a5f418404caf05e0f4ccfd
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-24 16:06:45 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,77 @@
-# your code goes here
+require 'pry'
+class Person
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    
+    #getter
+    def name
+        @name
+    end
+    #getter
+    def bank_account
+        @bank_account
+    end
+    #setter
+    def bank_account=(bank_account)
+        @bank_account = bank_account
+    end
+    #getter
+    def happiness
+        @happiness
+    end
+    #setter
+    def happiness=(happiness)
+        @happiness = happiness
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        end
+    end
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+    #getter
+    def hygiene
+        @hygiene
+    end
+    #setter
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        end
+    end
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+    
+    #getter
+    def get_paid(salary)
+        @salary
+        
+    end
+    #setter
+    binding.pry
+    def get_paid=(salary)
+        @salary = salary
+        @bank_account += @salary
+    end
+end
+# person_a = Person.new(“Shawn”)
+# puts person_a.name
+# puts person_a.bank_account
