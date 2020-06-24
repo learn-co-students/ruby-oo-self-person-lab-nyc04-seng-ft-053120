6720e8aa101b11bd41436d3c22fe2a285c35cfb8
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-06-24 13:17:19 -0400

    added more shit

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Person
     attr_reader :name,
     attr_accessor :balance, :happiness_points, :hygiene_points
@@ -8,14 +10,53 @@ class Person
         @happiness_points=happiness_points
         @hygiene_points=hygiene_points
     end
+
     def clean?
         self.hygiene_points>7
     end
+
     def happy?
         self.happiness_points>7
     end
+
     def get_paid(salary)
         self.balance+=salary
         "all about the benjamins"
     end
+
+    def take_bath
+        if hygiene_points < 7
+            self.hygiene_points += 4
+            "♪ Rub-a-dub just relaxing in the tub ♫"
+        end    
+    end
+    
+    def work_out
+        if hygiene_points >= 3
+            self.hygiene_points +- 3
+        end
+        
+        if happiness_points <= 8
+            self.happiness_points += 2
+        end
+
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        
+        if self.happiness_points <= 7
+            self.happiness_points += 3
+        end
+        
+        if friend.happiness_points <= 7
+            friend.happiness_points += 3
+        end
+        binding.pry
+
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+
+    end
+
 end
+
