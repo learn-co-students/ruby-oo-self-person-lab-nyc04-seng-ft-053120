2eb55a68ad5d53a55df9a9db7cafd2b258c57ea7
“DonatelloTHM” <donat.pllana@outlook.com>
2020-06-24 12:45:21 -0400

    Added get_paid

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,21 @@
-# your code goes here
-def hi
-    
+class Person
+    attr_reader :name,
+    attr_accessor :balance, :happiness_points, :hygiene_points
+
+    def initialize(name,balance,happiness_points,hygiene_points)
+        @name=name
+        @balance=balance
+        @happiness_points=happiness_points
+        @hygiene_points=hygiene_points
+    end
+    def clean?
+        self.hygiene_points>7
+    end
+    def happy?
+        self.happiness_points>7
+    end
+    def get_paid(salary)
+        self.balance+=salary
+        "all about the benjamins"
+    end
 end
