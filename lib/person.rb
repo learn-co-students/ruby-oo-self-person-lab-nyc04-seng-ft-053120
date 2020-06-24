d05826922709721c294c47b91dad6dc3638449e9
kceron <karemiceron@gmail.com>
2020-06-24 15:11:06 -0400

    we have so far

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
-# your code goes here
+
+require 'pry'
+
+class Person
+    attr_reader :name 
+    attr_accessor :bank_account, :happiness 
+
+    def initialize(name, bank_account, happiness)
+        @name = name 
+        @bank_account = 25 
+        @happiness = 8
+        @hygiene = 8 
+    end
+#binding.pry
+
+
+
+
+end
